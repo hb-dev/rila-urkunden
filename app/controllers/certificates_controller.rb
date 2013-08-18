c7748b47a751efba89b9e81d0c24807a79cd5ab1
Henrik <dev@ubuntu.(none)>
2013-08-18 12:41:46 +0200

    as

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -4,7 +4,10 @@ class CertificatesController < ActionController::Base
   
   def show
     @data = params[:id].tr('-_','+/').unpack('m')[0]
-    #render  :pdf => "urkunde", :page_size  => 'A4', :margin => { :top => 0, :bottom => 0, :left => 0, :right => 0}
+    
+    Rails.env == "development" ? @bg_path = 'C:/Users/Henrik/My Documents/Aptana Studio 3 Workspace/2013_06_rila_urkunden/public/images/urkunde_2013_leer.jpg' : @bg_path = 'http://ringelnatzlauf.herokuapp.com/images/urkunde_2013_leer.jpg'    
+    
+     #render  :pdf => "urkunde", :page_size  => 'A4', :margin => { :top => 0, :bottom => 0, :left => 0, :right => 0}
  end
   
 end
